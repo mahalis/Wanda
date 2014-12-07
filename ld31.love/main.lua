7edba954f1f1f69bf3c4ef9d4ec94c9d95bb9697
Noah Witherspoon <mahalis@gmail.com>
2014-12-06 17:58:41 -0800

    Graphics and stuff

--- ld31.love/main.lua
+++ ld31.love/main.lua
@@ -10,12 +10,14 @@ local elapsedTime = 0
 local started = false
 
 WALL_THICKNESS = 20
-TARGET_SIZE = 30
+TARGET_SIZE = 20
 TARGET_DIE_TIME = 0.4
 NEW_TARGET_MIN_DISTANCE_FROM_PLAYER = 2 * TARGET_SIZE
 
 local score = 0
 local scoreChangedTime = -1
+
+local backgroundImage, targetImage, wandaImage
 
 -- need to keep track of fixtures for world callback collisions
 
@@ -42,25 +44,30 @@ end
 function love.load()
 	love.graphics.setBackgroundColor(56, 60, 64)
 	love.graphics.setLineStyle("smooth")
+
+	backgroundImage = love.graphics.newImage("graphics/background.jpg")
+	targetImage = love.graphics.newImage("graphics/ring.png")
+	wandaImage = love.graphics.newImage("graphics/wanda.png")
+
 	local w, h = love.window.getDimensions()
 	world = love.physics.newWorld(0, 400) -- second parameter is Y gravity
 	world:setCallbacks(contactBegan, nil, nil, nil)
 
 	leftWall = {}
-	leftWall.shape = love.physics.newRectangleShape(WALL_THICKNESS, h)
+	leftWall.shape = love.physics.newRectangleShape(WALL_THICKNESS, h * 1.5)
 	leftWall.body = love.physics.newBody(world, WALL_THICKNESS / 2, h / 2)
 	leftWall.fixture = love.physics.newFixture(leftWall.body, leftWall.shape)
 	leftWall.fixture:setRestitution(1)
 
 	rightWall = {}
-	rightWall.shape = love.physics.newRectangleShape(WALL_THICKNESS, h)
+	rightWall.shape = love.physics.newRectangleShape(WALL_THICKNESS, h * 1.5)
 	rightWall.body = love.physics.newBody(world, w - WALL_THICKNESS / 2, h / 2)
 	rightWall.fixture = love.physics.newFixture(rightWall.body, rightWall.shape)
 	rightWall.fixture:setRestitution(1)
 
 	floor = {}
 	floor.shape = love.physics.newRectangleShape(w - 2 * WALL_THICKNESS, WALL_THICKNESS)
-	floor.body = love.physics.newBody(world, w / 2, h - WALL_THICKNESS / 2)
+	floor.body = love.physics.newBody(world, w / 2, h * 1.25)
 	floor.fixture = love.physics.newFixture(floor.body, floor.shape)
 	floor.fixture:setRestitution(1)
 
@@ -71,7 +78,7 @@ function love.load()
 	ceiling.fixture:setRestitution(1)
 
 	bot = {}
-	bot.shape = love.physics.newCircleShape(10)
+	bot.shape = love.physics.newRectangleShape(50, 30)
 	bot.body = love.physics.newBody(world, w / 2, 100, "dynamic")
 	bot.fixture = love.physics.newFixture(bot.body, bot.shape)
 	bot.fixture:setRestitution(0.9)
@@ -83,23 +90,34 @@ end
 function love.draw()
 	love.graphics.setColor(255, 255, 255, 255)
 	local w, h = love.window.getDimensions()
+	--[[
 	drawWorldBox(floor)
 	drawWorldBox(leftWall)
 	drawWorldBox(rightWall)
 	drawWorldBox(ceiling)
+	]]
+	love.graphics.draw(backgroundImage, 0, 0)
 
+	local ringW, ringH = targetImage:getDimensions()
 	for i = 1, #targets do
 		local target = targets[i]
 		local targetBumpAmount = 1 - math.max(math.min((elapsedTime - target.lastTouchTime) / TARGET_DIE_TIME, 1), 0)
-		love.graphics.setColor(40, 190 + 60 * targetBumpAmount, 0, 100 + 100 * targetBumpAmount)
-		love.graphics.circle("fill", target.body:getX(), target.body:getY(), TARGET_SIZE)
+		love.graphics.setColor(40, 190 + 60 * targetBumpAmount, 0, 240 * targetBumpAmount)
+		local x = target.body:getX()
+		local y = target.body:getY()
+		love.graphics.circle("fill", x, y, TARGET_SIZE * 1.2)
+		love.graphics.setColor(255, 255, 255, 255)
+		love.graphics.draw(targetImage, x, y, elapsedTime * 0.5, 1, 1, ringW / 2, ringH / 2)
 	end
 
 	love.graphics.setColor(255, 255, 255, 255)
-	love.graphics.circle("line", bot.body:getX(), bot.body:getY(), bot.shape:getRadius(), 20)
+	local wandaW, wandaH = wandaImage:getDimensions()
+	love.graphics.draw(wandaImage, bot.body:getX(), bot.body:getY(), bot.body:getAngle(), 1, 1, wandaW / 2, wandaH / 2)
+
 	if anchor.joint then
 		love.graphics.circle("line", anchor.body:getX(), anchor.body:getY(), 4, 20)
-		love.graphics.line(anchor.body:getX(), anchor.body:getY(), bot.body:getX(), bot.body:getY())
+		local mouthX, mouthY = bot.body:getWorldPoint(0, 0)---32,3)
+		love.graphics.line(anchor.body:getX(), anchor.body:getY(), mouthX, mouthY)
 	end
 
 	love.graphics.printf(string.format("%03d", score), w - 100, 40, 60, "right")
@@ -142,15 +160,17 @@ end
 function makeAnchor(x, y)
 	breakAnchor()
 	anchor.body:setPosition(x,y)
-	local botX, botY = bot.body:getX(), bot.body:getY()
+	local botX, botY = bot.body:getWorldPoint(0, 0)
 	anchor.joint = love.physics.newDistanceJoint(anchor.body, bot.body, x, y, botX, botY)
 
 	-- apply force in the current direction of movement but orthogonal to the joint
 	local toBot = vNorm(vSub(v(botX, botY), v(x, y)))
 	local velX, velY = bot.body:getLinearVelocity()
 	local botDirection = vNorm(v(velX, velY))
-	local impulse = vNorm(vSub(botDirection, vMul(toBot, vDot(botDirection, toBot))), 100)
+	local impulse = vNorm(vSub(botDirection, vMul(toBot, vDot(botDirection, toBot))), 400)
 	bot.body:applyLinearImpulse(impulse.x, impulse.y)
+
+	bot.body:applyAngularImpulse((math.random() * 2 - 1) * 2000)
 
 	adjustScore(-1)
 end
